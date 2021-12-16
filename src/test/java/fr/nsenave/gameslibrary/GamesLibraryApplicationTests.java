edf5c8940a00bac3458e2180065fc9d0cefcd4b1
nsenave <nicolas.senave@gmail.com>
2021-12-16 17:48:52 +0100

    create step tests

--- src/test/java/fr/nsenave/gameslibrary/GamesLibraryApplicationTests.java
+++ src/test/java/fr/nsenave/gameslibrary/GamesLibraryApplicationTests.java
@@ -1,13 +1,74 @@
 package fr.nsenave.gameslibrary;
 
+import fr.nsenave.gameslibrary.jobs.BatchConfig;
+import fr.nsenave.gameslibrary.jobs.JobCompletionNotificationListener;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.*;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.JobRepositoryTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 
-@SpringBootTest
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBatchTest
+@EnableAutoConfiguration
+@ContextConfiguration(classes = {
+		JobCompletionNotificationListener.class,
+		BatchConfig.class})
+/*@TestExecutionListeners({
+		DependencyInjectionTestExecutionListener.class,
+		DirtiesContextTestExecutionListener.class})*/
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 class GamesLibraryApplicationTests {
 
+	public static final String SINGLE_FILE = "src/test/resources/games_test.json";
+	public static final String DATA_DIRECTORY = "src/test/resources/test_files";
+
+	@Autowired
+	private JobLauncherTestUtils jobLauncherTestUtils;
+
+	@Autowired
+	private JobRepositoryTestUtils jobRepositoryTestUtils;
+
+	@AfterEach
+	public void cleanUp() {
+		jobRepositoryTestUtils.removeJobExecutions();
+	}
+
+	private JobParameters defaultJobParameters() {
+		JobParametersBuilder paramsBuilder = new JobParametersBuilder();
+		paramsBuilder.addString("jsonFilePath", SINGLE_FILE);
+		paramsBuilder.addString("directoryPath", DATA_DIRECTORY);
+		return paramsBuilder.toJobParameters();
+	}
+
 	@Test
-	void contextLoads() {
+	public void testStep2() {
+		JobExecution jobExecution = jobLauncherTestUtils.launchStep(
+				"step2", defaultJobParameters());
+		ExitStatus actualExitStatus = jobExecution.getExitStatus();
+		assertEquals(actualExitStatus.getExitCode(), BatchStatus.COMPLETED.name());
+	}
+
+	@Test
+	public void testStep3() throws IOException {
+		JobExecution jobExecution = jobLauncherTestUtils.launchStep(
+				"step3", defaultJobParameters());
+		ExitStatus actualExitStatus = jobExecution.getExitStatus();
+		assertEquals(actualExitStatus.getExitCode(), BatchStatus.COMPLETED.name());
 	}
 
 }
