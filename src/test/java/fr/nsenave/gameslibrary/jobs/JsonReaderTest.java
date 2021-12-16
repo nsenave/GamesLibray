f056d1bc3072e8a1b5941bda65dbeb0cb0bed14f
nsenave <nicolas.senave@gmail.com>
2021-12-16 17:43:34 +0100

    from sysout to test

--- src/test/java/fr/nsenave/gameslibrary/jobs/JsonReaderTest.java
+++ src/test/java/fr/nsenave/gameslibrary/jobs/JsonReaderTest.java
@@ -5,13 +5,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.nsenave.gameslibrary.buisness.Game;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class JsonReaderTest {
 
+    /**
+     * just to be sure that we use Jackson's mapper correctly
+     */
     @Test
     public void testMapJsonFile() throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         String jsonString = "{\"name\": \"n1\", \"console\": \"c1\", \"release\": 2000}";
         Game game = mapper.readValue(jsonString, Game.class);
-        System.out.println(game);
+        assertEquals("n1", game.getName());
+        assertEquals("c1", game.getConsole());
+        assertEquals(2000, game.getRelease());
     }
 }
